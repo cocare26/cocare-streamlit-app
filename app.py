9d9f07516085d7ecc412eb71c73aacc213f8fafb
cocare26 <co.care26@gmail.com>
2026-05-02 15:44:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -117,9 +117,7 @@ st.markdown('</div>', unsafe_allow_html=True)حطلي <script>
 function login(){
     const v = document.getElementById("username").value;
     const e = document.getElementById("error");
-
-    // يقبل Phone يبدأ بـ 07 وطوله 10
-    // أو ID طوله 11
+ 
     if(/^07[0-9]{8}$/.test(v) || /^[0-9]{11}$/.test(v)){
         window.top.location.href = "/?page=employee";
     } else {
