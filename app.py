e458b0b46aefddc93828697664dea8641867d801
cocare26 <co.care26@gmail.com>
2026-05-02 15:27:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ function login(){
     const v = document.getElementById("username").value;
     const e = document.getElementById("error");
 
-    // يقبل Phone يبدأ بـ 07 وطوله 10
-    // أو ID طوله 11
     if(/^07[0-9]{8}$/.test(v) || /^[0-9]{11}$/.test(v)){
         window.top.location.href = "/?page=employee";
     } else {
