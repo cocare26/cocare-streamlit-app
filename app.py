8c8bf0c7a42399b7884ffe78066dc7a4605c9b36
cocare26 <co.care26@gmail.com>
2026-05-02 18:12:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,14 @@ page = st.query_params.get("page", "")
 if page == "create":
     st.switch_page("pages/1_Create_Account.py")
 
+elif page == "customer":
+    st.switch_page("pages/2_Customer.py")
+
 elif page == "employee":
     st.switch_page("pages/3_Employee.py")
 
 elif page == "forgot":
-    st.switch_page("pages/2_Forgot_Password.py")
+    st.switch_page("pages/4_Forgot_Password.py")
 
 with open("robot.png", "rb") as f:
     img = base64.b64encode(f.read()).decode()
@@ -25,88 +28,96 @@ html = """
 body{margin:0;background:#eef3f6;font-family:Arial}
 .phone{
 width:360px;height:660px;margin:auto;border-radius:42px;overflow:hidden;
-position:relative;background:linear-gradient(160deg,#d6ecff,#bfe3ff,#eaf6ff)
+position:relative;background:linear-gradient(180deg,#c9e7f7,#dff4ff)
 }
-.robot{
-position:absolute;
-top:140px;
-right:-20px;
-width:130px;
-z-index:3;
-transform:translateY(-35%);
-}
-.form{position:absolute;top:200px;left:58px;width:244px}
+.robot{position:absolute;top:85px;left:168px;width:145px;z-index:3}
+.form{position:absolute;top:200px;left:58px;width:244px;z-index:2}
 .input{
-width:100%;height:44px;border-radius:25px;margin-bottom:13px;
-padding-left:18px;border:none;background:white;
-box-shadow:0 6px 16px rgba(0,0,0,0.08)
+width:100%;height:40px;border-radius:25px;margin-bottom:13px;
+padding-left:18px;border:none;background:white;box-sizing:border-box
 }
+.forgot{text-align:center;font-size:11px;color:#555;margin:8px 0 20px;cursor:pointer}
 .login{
-width:100%;height:46px;border-radius:25px;
-background:linear-gradient(90deg,#2f80ed,#1c6fa4);
-color:white;font-weight:bold;border:none;cursor:pointer
+width:100%;height:46px;border-radius:25px;background:white;
+text-align:center;line-height:46px;font-weight:bold;border:none;cursor:pointer
 }
-.forgot a,
-.signup a{
+.signup{
 display:block;
-width:100%;
-height:42px;
-line-height:42px;
-border-radius:25px;
-margin-top:10px;
-background:linear-gradient(90deg,#2f80ed,#1c6fa4);
-color:white;
 text-align:center;
+font-size:13px;
+margin-top:15px;
+cursor:pointer;
+color:#222;
 text-decoration:none;
-font-weight:700;
 }
-.error{text-align:center;color:red;font-size:12px;margin-top:8px}
+.error{text-align:center;color:#c62828;font-size:11px;margin-top:8px}
+
+.signup-line{
+    text-align:center;
+    font-size:13px;
+    margin-top:15px;
+    color:#222;
+}
+
+.signup-line span{
+    color:#1c6fa4;
+    font-weight:bold;
+    text-decoration:none;
+    margin-left:4px;
+    cursor:pointer;
+}
 </style>
 </head>
 
 <body>
 <div class="phone">
-
 <img class="robot" src="data:image/png;base64,IMG_HERE">
 
-<div class="form">
+<form class="form" id="loginForm" method="get" target="_top">
+    <input type="hidden" name="page" id="pageValue">
 
-<input id="username" class="input" placeholder="phone / ID Number"
-inputmode="numeric" maxlength="11"
-oninput="this.value=this.value.replace(/[^0-9]/g,'')">
+    <input id="username" class="input" placeholder="phone / ID Number"
+    inputmode="numeric" maxlength="11"
+    oninput="this.value=this.value.replace(/[^0-9]/g,'')">
 
-<input class="input" placeholder="Password" type="password">
+    <input class="input" placeholder="Password" type="password">
 
-<div class="forgot">
-<a href="/?page=forgot" target="_top">Forgot Password?</a>
+    <div class="forgot">
+    <a href="/?page=forgot" target="_self" style="color:#555; text-decoration:none;">
+        Forgot Password?
+    </a>
 </div>
-
-<button class="login" onclick="login()">Log In ›</button>
-
-<div id="error" class="error"></div>
-
-<div class="signup">
-<a href="/?page=create" target="_top">Create Account</a>
+    <button class="login" type="button" onclick="login()">Log In ›</button>
+    <div id="error" class="error"></div>
+    <div class="signup">
+    👤 New User?
+    <a href="/?page=create" target="_self" style="color:#222; text-decoration:underline;">
+        Create Account
+    </a>
 </div>
-
-</div>
+</form>
 </div>
 
 <script>
+function goPage(p){
+    window.top.location.href = "/?page=" + p;
+}
+
 function login(){
     const v = document.getElementById("username").value;
     const e = document.getElementById("error");
 
-    // 🔥 دخول مباشر للموظف
-    if(/^[0-9]{11}$/.test(v)){
-        window.parent.location.href = "?page=employee";
-    } 
+    if(/^07[0-9]{8}$/.test(v)){
+        goPage("customer");
+    }
+    else if(/^[0-9]{11}$/.test(v)){
+        goPage("employee");
+    }
     else{
-        e.innerText = "Enter valid ID (11 digits)";
+        e.innerText = "Invalid phone or ID number";
     }
 }
 </script>
-
 </body>
 </html>
 """
