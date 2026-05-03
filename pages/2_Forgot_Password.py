5b59085824acbf9f6e46057e9cec10555b5cf8e2
cocare26 <co.care26@gmail.com>
2026-05-03 23:43:37 +0300

    2_Forgot_Password.py

--- pages/2_Forgot_Password.py
+++ pages/2_Forgot_Password.py
@@ -77,16 +77,16 @@ div.stButton:nth-of-type(3) > button {
 
 st.title("Forgot Password")
 
-phone = st.text_input("Phone Number or ID", max_chars=11)
+phone = st.text_input("Phone Number ", max_chars=11)
 
 if st.button("Send Code"):
     if phone.isdigit() and (
-        (len(phone) == 10 and phone.startswith("07")) or len(phone) == 11
+        (len(phone) == 10 and phone.startswith("07")) 
     ):
         st.session_state.reset_code = str(random.randint(1000, 9999))
         st.success(f"Reset code: {st.session_state.reset_code}")
     else:
-        st.error("Enter valid phone or ID")
+        st.error("Enter valid phone ")
 
 code = st.text_input("Enter Code")
 new_password = st.text_input("New Password", type="password")
