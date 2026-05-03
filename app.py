5a269b7815f0b1f301f0606cda2149a7d57e7572
cocare26 <co.care26@gmail.com>
2026-05-04 01:17:53 +0300

    app.py

--- app.py
+++ app.py
@@ -5,164 +5,122 @@ import base64
 st.set_page_config(page_title="Telecom App", layout="centered")
 
 page = st.query_params.get("page", "")
-if isinstance(page, list):
-    page = page[0]
-page = page or ""
 
 if page == "create":
     st.switch_page("pages/1_Create_Account.py")
+
 elif page == "customer":
     st.switch_page("pages/2_Customer.py")
+
 elif page == "employee":
     st.switch_page("pages/3_Employee.py")
-elif page == "todo":
-    st.switch_page("pages/4_To_Do_List.py")
-elif page == "forgot":
-    st.switch_page("pages/2_Forgot_Password.py")
-elif page == "customer_support":
-    st.switch_page("pages/5_Customer_Support_Arabic.py")
-elif page == "network_diagnostics":
-    st.switch_page("pages/6_Network_Diagnostics_Arabic.py")
-elif page == "customer_management":
-    st.switch_page("pages/7_Customer_Management_Arabic.py")
-elif page == "employee_performance":
-    st.switch_page("pages/8_Employee_Performance_Arabic.py")
-elif page == "service_management":
-    st.switch_page("pages/9_Service_Management_Arabic.py")
-elif page == "settings":
-    st.switch_page("pages/10_Settings_Arabic.py")
-elif page == "notifications":
-    st.switch_page("pages/11_Notifications_Arabic.py")
-elif page == "change_password":
-    st.switch_page("pages/Change_Password.py")
-elif page == "change_language":
-    st.switch_page("pages/Change_Language.py")
-elif page == "report_problem":
-    st.switch_page("pages/Report_Problem.py")
-elif page == "contact_us":
-    st.switch_page("pages/Contact_Us.py")
-elif page == "rate_app":
-    st.switch_page("pages/Rate_App.py")
-elif page == "settings_en":
-    st.switch_page("pages/setting.py")
-elif page == "settings_ar":
-    st.switch_page("pages/setting-ar.py")
 
-st.markdown("""
-<style>
-[data-testid="stAppViewContainer"]{
-    background:#eef2f7;
-}
-.block-container{
-    padding-top:20px;
-}
-header, footer{
-    visibility:hidden;
-}
-</style>
-""", unsafe_allow_html=True)
+elif page == "forgot":
+    st.switch_page("pages/4_Forgot_Password.py")
 
 with open("robot.png", "rb") as f:
     img = base64.b64encode(f.read()).decode()
 
-html = f"""
+html = """
 <html>
-<body style="margin:0;background:#eef3f6;font-family:Arial">
+<head>
+<style>
+body{margin:0;background:#eef3f6;font-family:Arial}
+.phone{
+width:360px;height:660px;margin:auto;border-radius:42px;overflow:hidden;
+position:relative;background:linear-gradient(180deg,#c9e7f7,#dff4ff)
+}
+.robot{position:absolute;top:85px;left:168px;width:145px;z-index:3}
+.form{position:absolute;top:200px;left:58px;width:244px;z-index:2}
+.input{
+width:100%;height:40px;border-radius:25px;margin-bottom:13px;
+padding-left:18px;border:none;background:white;box-sizing:border-box
+}
+.forgot{text-align:center;font-size:11px;color:#555;margin:8px 0 20px;cursor:pointer}
+.login{
+width:100%;height:46px;border-radius:25px;background:white;
+text-align:center;line-height:46px;font-weight:bold;border:none;cursor:pointer
+}
+.signup{
+display:block;
+text-align:center;
+font-size:13px;
+margin-top:15px;
+cursor:pointer;
+color:#222;
+text-decoration:none;
+}
+.error{text-align:center;color:#c62828;font-size:11px;margin-top:8px}
 
-<div style="
-width:360px;
-height:660px;
-margin:auto;
-border-radius:42px;
-background:linear-gradient(180deg,#c9e7f7,#dff4ff);
-position:relative;
-overflow:hidden;
-">
+.signup-line{
+    text-align:center;
+    font-size:13px;
+    margin-top:15px;
+    color:#222;
+}
 
-<img src="data:image/png;base64,{img}" 
-style="position:absolute;top:85px;left:110px;width:140px">
+.signup-line span{
+    color:#1c6fa4;
+    font-weight:bold;
+    text-decoration:none;
+    margin-left:4px;
+    cursor:pointer;
+}
+</style>
+</head>
 
-<div style="position:absolute;top:250px;left:60px;width:240px;">
+<body>
+<div class="phone">
+<img class="robot" src="data:image/png;base64,IMG_HERE">
 
-<input id="username" placeholder="phone / ID"
-style="
-width:100%;
-height:40px;
-border-radius:25px;
-margin-bottom:12px;
-border:none;
-padding-left:15px;
-">
+<form class="form" id="loginForm" method="get" target="_top">
+    <input type="hidden" name="page" id="pageValue">
 
-<input id="password" type="password" placeholder="Password"
-style="
-width:100%;
-height:40px;
-border-radius:25px;
-margin-bottom:12px;
-border:none;
-padding-left:15px;
-">
+    <input id="username" class="input" placeholder="phone / ID Number"
+    inputmode="numeric" maxlength="11"
+    oninput="this.value=this.value.replace(/[^0-9]/g,'')">
 
-<div style="text-align:center;font-size:12px;margin-bottom:15px;">
-    <a href="/?page=forgot" target="_top" style="color:#333;text-decoration:none;">
+    <input class="input" placeholder="Password" type="password">
+
+    <div class="forgot">
+    <a href="/?page=forgot" target="_self" style="color:#555; text-decoration:none;">
         Forgot Password?
     </a>
 </div>
-
-<button onclick="login()"
-style="
-width:100%;
-height:45px;
-border-radius:25px;
-border:none;
-font-weight:bold;
-background:white;
-color:#0f2446;
-cursor:pointer;
-">
-Log In
-</button>
-
-<p id="error" style="color:red;text-align:center;font-size:12px;"></p>
-
-<div style="text-align:center;font-size:13px;margin-top:12px;">
+    <button class="login" type="button" onclick="login()">Log In ›</button>
+    <div id="error" class="error"></div>
+    <div class="signup">
     👤 New User?
-    <a href="/?page=create" target="_top" style="
-        color:#0f2446;
-        font-weight:bold;
-        text-decoration:underline;
-    ">
+    <a href="/?page=create" target="_self" style="color:#222; text-decoration:underline;">
         Create Account
     </a>
 </div>
-
-</div>
+</form>
 </div>
 
 <script>
-function go(p){{
+function goPage(p){
     window.top.location.href = "/?page=" + p;
-}}
+}
 
-function login(){{
-    let v = document.getElementById("username").value;
-    let e = document.getElementById("error");
+function login(){
+    const v = document.getElementById("username").value;
+    const e = document.getElementById("error");
 
-    if(/^07[0-9]{{8}}$/.test(v)){{
-        go("customer");
-    }}
-    else if(/^[0-9]{{11}}$/.test(v)){{
-        go("employee");
-    }}
-    else{{
-        e.innerText = "Invalid phone or ID";
-    }}
-}}
+    if(/^07[0-9]{8}$/.test(v)){
+        goPage("customer");
+    }
+    else if(/^[0-9]{11}$/.test(v)){
+        goPage("employee");
+    }
+    else{
+        e.innerText = "Invalid phone or ID number";
+    }
+}
 </script>
-
 </body>
 </html>
 """
 
+html = html.replace("IMG_HERE", img)
 components.html(html, height=700)
