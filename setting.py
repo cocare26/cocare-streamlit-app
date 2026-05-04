11a998143d49a65f5783fa65ab417c2694b44e2c
cocare26 <co.care26@gmail.com>
2026-05-04 17:18:57 +0300

    Update setting.py

--- setting.py
+++ setting.py
@@ -183,11 +183,11 @@ else:
         <head>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
             <style>
-                body {
+                body {{
                     font-family: 'Segoe UI', sans-serif;
                     background: transparent;
                     margin: 0;
-                }
+                }}
 
                 .main-wrapper {
                     width: 100%;
