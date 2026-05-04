56c7f693fa813770286f6ef3256846d6c0e5ffe1
cocare26 <co.care26@gmail.com>
2026-05-04 19:17:35 +0300

    Update settingnew.py

--- settingnew.py
+++ settingnew.py
@@ -142,7 +142,8 @@ if st.session_state.page == "main":
         go("rate")
 
     if st.button(f"🚪 {T['logout']}"):
-        st.session_state.clear() st.switch_page("app.py")
+        st.session_state.clear() 
+        st.switch_page("app.py")
 
     col1, col2 = st.columns(2)
 
