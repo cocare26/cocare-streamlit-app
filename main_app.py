acc2e952489b61652713e05cc3c0b43717a8eb6c
cocare26 <co.care26@gmail.com>
2026-05-04 23:16:03 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,31 +2,24 @@ import streamlit as st
 
 st.set_page_config(page_title="App", layout="centered")
 
-# قراءة الصفحة من الرابط
 page = st.query_params.get("page", "")
 
-# ========= SETTINGS ROUTER =========
+# ========= SETTINGS =========
 if page == "settings":
-    st.switch_page("pages/Settings.py")
+    st.switch_page("5 setting")
 
-elif page == "Change_password-ar":
-    st.switch_page("pages/Change_password-ar.py")
+elif page == "change_password":
+    st.switch_page("6 Change Password")
 
-elif page == "Change_language-ar":
-    st.switch_page("pages/Change_language-ar.py")
+elif page == "change_language":
+    st.switch_page("7 Change Language")
 
-elif page == "Rate_app-ar":
-    st.switch_page("pages/Rate_app-ar.py")
+elif page == "contact":
+    st.switch_page("10 Contact Us")
 
-elif page == "Report_Problem-ar":
-    st.switch_page("pages/Report_Problem-ar.py")
-
-elif page == "Contact_Us-ar":
-    st.switch_page("pages/Contact_Us-ar.py")
-
-elif page == "logout":
-    st.switch_page("pages/logout.py")
+elif page == "report":
+    st.switch_page("11 settingar")
 
 # ========= DEFAULT =========
 else:
-    st.switch_page("pages/Settings.py")
+    st.switch_page("5 setting")
