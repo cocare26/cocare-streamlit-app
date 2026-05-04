8675e68631046ed564f236250fcfc2060b605378
cocare26 <co.care26@gmail.com>
2026-05-04 22:44:41 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,17 +1,11 @@
-import streamlit as st
-
-st.set_page_config(layout="centered")
-
 def navigate():
-
     params = st.query_params
     page = params.get("page", "login")
 
     if isinstance(page, list):
         page = page[0]
 
-
-    elif page == "customer":
+    if page == "customer":
         st.switch_page("pages/2_Customer.py")
 
     elif page == "settings-ar":
@@ -39,7 +33,3 @@ def navigate():
         st.session_state.clear()
         st.query_params.clear()
         st.switch_page("pages/0_arabic_app.py")
-
-
-if __name__ == "__main__":
-    navigate()
