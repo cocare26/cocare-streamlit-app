4d28ed6ed39a19bb25c8e1d0342923b4835a5ab2
cocare26 <co.care26@gmail.com>
2026-05-04 22:37:39 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -3,45 +3,46 @@ import streamlit as st
 st.set_page_config(layout="centered")
 
 def navigate():
-    # ✅ استخدام الطريقة الحديثة
+
     params = st.query_params
     page = params.get("page", "login")
 
-    # لأن القيمة ممكن تكون list
     if isinstance(page, list):
         page = page[0]
 
+    # 🔥 IMPORTANT: switch_page يحتاج اسم الملف داخل pages بدون "pages/"
+    
     if page == "login":
-        st.switch_page("pages/0_arabic_app.py")
+        st.switch_page("0_arabic_app")
 
     elif page == "customer":
-        st.switch_page("pages/2_Customer.py")
+        st.switch_page("2_Customer")
 
     elif page == "settings-ar":
-        st.switch_page("pages/11_settingar.py")
+        st.switch_page("11_settingar")
 
     elif page == "settings-en":
-        st.switch_page("pages/5_setting.py")
+        st.switch_page("5_setting")
 
     elif page == "Change_password-ar":
-        st.switch_page("pages/6_Change_Password.py")
+        st.switch_page("6_Change_Password")
 
     elif page == "Change_language-ar":
-        st.switch_page("pages/7_Change_Language.py")
+        st.switch_page("7_Change_Language")
 
     elif page == "Rate_app-ar":
-        st.switch_page("pages/8_Rate_App.py")
+        st.switch_page("8_Rate_App")
 
     elif page == "Report_Problem-ar":
-        st.switch_page("pages/9_Report_Problem.py")
+        st.switch_page("9_Report_Problem")
 
     elif page == "Contact_Us-ar":
-        st.switch_page("pages/10_Contact_Us.py")
+        st.switch_page("10_Contact_Us")
 
     elif page == "logout":
         st.session_state.clear()
         st.query_params.clear()
-        st.switch_page("pages/0_arabic_app.py")
+        st.switch_page("0_arabic_app")
 
 
 if __name__ == "__main__":
