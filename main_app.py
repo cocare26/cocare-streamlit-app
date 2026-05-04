abddec999814fb7e544f80d59e5343cb2890a2be
cocare26 <co.care26@gmail.com>
2026-05-05 00:02:55 +0300

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -2,30 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="App", layout="centered")
 
-# ✅ قراءة query params بشكل صحيح
-page = st.query_params.get("page")
-
-if page:
-    page = page[0]
-else:
-    page = ""
-
-# ========= ROUTER =========
-if page == "settings":
-    st.switch_page("pages/Settings.py")
-
-elif page == "change_password":
-    st.switch_page("pages/ChangePassword.py")
-
-elif page == "change_language":
-    st.switch_page("pages/ChangeLanguage.py")
-
-elif page == "contact":
-    st.switch_page("pages/ContactUs.py")
-
-elif page == "report":
-    st.switch_page("pages/ReportProblem.py")
-
-# ========= DEFAULT =========
-else:
-    st.switch_page("pages/Settings.py")
+# مباشرة يفتح Settings
+st.switch_page("pages/Settings.py")
