f16054b4927f4695e01375ab4f3aee2ba704680a
cocare26 <co.care26@gmail.com>
2026-05-05 07:23:10 +0300

    Update ChangePassword.py

--- pages/ChangePassword.py
+++ pages/ChangePassword.py
@@ -113,6 +113,7 @@ body {
     font-weight:900;
     cursor:pointer;
     box-shadow:0 4px 12px rgba(0,0,0,0.08);
+    transition: transform 0.25s ease;
 }
 .save button:hover {
     transform: translateY(-5px);
