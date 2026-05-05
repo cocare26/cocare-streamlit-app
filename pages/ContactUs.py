1b9bace60b63353335fc640936a4d3bba2199581
cocare26 <co.care26@gmail.com>
2026-05-05 10:57:36 +0300

    Update ContactUs.py

--- pages/ContactUs.py
+++ pages/ContactUs.py
@@ -87,7 +87,7 @@ st.markdown('<h2 style="text-align:center; color:#102646; font-weight:900; margi
 # ===== الأزرار بنفس المسافات العملاقة =====
 
 # مسافة كبيرة جداً عشان تندفع النصوص لليمين وتصير "مسطرة"
-contact_gap = "&nbsp;" * 35
+gap = "&nbsp;" * 4
 
 st.button(f"✉️{contact_gap}Email: Co.Care26@gmail.com")
 
