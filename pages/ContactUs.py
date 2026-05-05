b87bda8c299de8e40d5cc1490d989811302fc76b
cocare26 <co.care26@gmail.com>
2026-05-05 11:45:37 +0300

    Update ContactUs.py

--- pages/ContactUs.py
+++ pages/ContactUs.py
@@ -72,6 +72,6 @@ spaces = " " * 35
 
 if st.button(f"✉️ Email: Co.Care26@gmail.com{spacesspaces}"):
     pass
-
-if st.button(f"📞 Phone: +962 79 123 4567{spacesspacesspaces}"):
+spaces = " " * 40 
+if st.button(f"📞 Phone: +962 79 123 4567{spacesspacesspacesspaces}"):
     pass
