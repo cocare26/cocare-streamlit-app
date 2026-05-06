e299dbd61e55ab624362c9393668e15f08a6644f
cocare26 <co.care26@gmail.com>
2026-05-06 19:32:37 +0300

    Create language_guard.py

---
+++ language_guard.py
@@ -0,0 +1,35 @@
+import streamlit as st
+
+AR_TO_EN = {
+
+    "settingar.py": "pages/Settings.py",
+    "Change_Languagear.py": "pages/ChangeLanguage.py",
+    "Change_Passwordar.py": "pages/ChangePassword.py",
+    "ContactUsar.py": "pages/ContactUs.py",
+    "Rate_Appar.py": "pages/RateApp.py",
+    "Report_Problemar.py": "pages/ReportProblem.py",
+    "Customer_ar.py": "pages/2_Customer.py",
+
+}
+
+EN_TO_AR = {
+
+    "Settings.py": "pages/settingar.py",
+    "ChangeLanguage.py": "pages/Change_Languagear.py",
+    "ChangePassword.py": "pages/Change_Passwordar.py",
+    "ContactUs.py": "pages/ContactUsar.py",
+    "RateApp.py": "pages/Rate_Appar.py",
+    "ReportProblem.py": "pages/Report_Problemar.py",
+    "2_Customer.py": "pages/Customer_ar.py",
+
+}
+
+def check_language(current_file):
+
+    lang = st.session_state.get("lang", "en")
+
+    if lang == "ar" and current_file in EN_TO_AR:
+        st.switch_page(EN_TO_AR[current_file])
+
+    elif lang == "en" and current_file in AR_TO_EN:
+        st.switch_page(AR_TO_EN[current_file])
