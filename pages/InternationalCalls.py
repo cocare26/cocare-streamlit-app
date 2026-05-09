54ca65df12f86bf72bfe4ae7c1d5492a8cec7516
cocare26 <co.care26@gmail.com>
2026-05-09 21:13:43 +0300

    Update InternationalCalls.py

--- pages/InternationalCalls.py
+++ pages/InternationalCalls.py
@@ -10,7 +10,7 @@ st.set_page_config(
 col1, col2 = st.columns([1, 8])
 with col1:
     if st.button("← Back"):
-        st.switch_page("pages/2_Customer.py")
+        st.switch_page("pages/2_Customer_EN.py")
 
 html_code = """
 <!DOCTYPE html>
