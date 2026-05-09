b5076bc9a8ff644447b642d3621c7001cd6ca4e1
cocare26 <co.care26@gmail.com>
2026-05-09 15:21:04 +0300

    Update 2_Customer.py

--- pages/2_Customer.py
+++ pages/2_Customer.py
@@ -378,40 +378,27 @@ st.markdown("""
 # =====================================
 # 6. الشريط السفلي (معدل: صور فقط)
 # =====================================
-st.markdown(f"""
+st.markdown(f""" 
 <div class="nav">
-
-<a href="/Settings" target="_self" style="text-decoration:none; color:inherit;">
-    <div class="nav-item clickable">
-        <img src="data:image/png;base64,{icon_sitting}" class="nav-img-footer">
-    </div>
-</a>
-
-<a href="/Game_E" target="_self" style="text-decoration:none; color:inherit;">
-    <div class="nav-item clickable">
-        <img src="data:image/png;base64,{icon_spin}" class="nav-img-footer">
-    </div>
-</a>
-
-<a href="/Chatbot" target="_self" style="text-decoration:none; color:inherit;">
-    <div class="nav-item clickable">
-        <div class="bot-bg">
-            <img src="data:image/png;base64,{robot_head}" style="width:34px;">
-        </div>
-    </div>
-</a>
-
-<a href="/Customer" target="_self" style="text-decoration:none; color:inherit;">
-    <div class="nav-item active clickable">
-        <img src="data:image/png;base64,{icon_home}" class="nav-img-footer">
-    </div>
-</a>
-
-<a href="/Game" target="_self" style="text-decoration:none; color:inherit;">
-    <div class="nav-item clickable">
-        <img src="data:image/png;base64,{icon_game}" class="nav-img-footer">
-    </div>
-</a>
-
+<div class="nav-item clickable">
+<img src="data:image/png;base64,{icon_sitting}"
+class="nav-img-footer">
+</div> 
+<div class="nav-item clickable">
+<img src="data:image/png;base64,{icon_spin}" 
+class="nav-img-footer"> 
+</div> 
+<div class="nav-item clickable"> 
+<div class="bot-bg"><img src="data:image/png;base64,{robot_head}" 
+style="width:34px;"></div>
+</div> 
+<div class="nav-item active clickable">
+<img src="data:image/png;base64,{icon_home}"
+class="nav-img-footer"> 
+</div> 
+<div class="nav-item clickable"> 
+<img src="data:image/png;base64,{icon_game}" 
+class="nav-img-footer"> 
+</div> 
 </div>
 """, unsafe_allow_html=True)
