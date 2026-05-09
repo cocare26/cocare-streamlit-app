146bee58f0a7bf5566af0b453e25649a4f70c47c
cocare26 <co.care26@gmail.com>
2026-05-09 16:26:57 +0300

    Update InternetPackages.py

--- pages/InternetPackages.py
+++ pages/InternetPackages.py
@@ -13,7 +13,7 @@ col1, col2 = st.columns([1, 8])
 
 with col1:
     if st.button("← Back"):
-        st.switch_page("2_Customerزحغ")
+        st.switch_page("2_Customer.py")
         # إذا داخل pages استخدم:
         # st.switch_page("pages/2_Customer.py")
 
