de6450fc1b0152dd2ffe959a3bc2837cdb059694
cocare26 <co.care26@gmail.com>
2026-05-10 00:09:39 +0300

    Update Report_Problemar.py

--- pages/Report_Problemar.py
+++ pages/Report_Problemar.py
@@ -73,8 +73,6 @@ footer {
 /* العنوان في المنتصف */
 .title-text {
     position: absolute;
-    left: 50%;
-    top: 50%;
     transform: translate(-50%, -150%);
     font-size: 20px;
     font-weight: 900;
