132f7972dcd0d1d69299bef4a87dffbfafdc4727
cocare26 <co.care26@gmail.com>
2026-05-14 12:22:38 +0300

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,9 @@ import sqlite3
 DB_NAME = "app.db"
 
 def get_connection():
-    return sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect(DB_NAME, check_same_thread=False)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 def init_db():
     conn = get_connection()
@@ -12,14 +14,10 @@ def init_db():
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
+        name TEXT,
         email TEXT UNIQUE
     )
     """)
 
     conn.commit()
     conn.close()
-
-if __name__ == "__main__":
-    init_db()
-    print("DB created")
